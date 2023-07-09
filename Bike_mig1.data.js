6beccda9becf16ef3d74f2102e8657335b343dc8
dmitriymorozovru <118219809+dmitriymorozovru@users.noreply.github.com>
2023-07-09 14:18:15 +0300

    Update Bike_mig1.data.js

--- Bike_mig1.data.js
+++ Bike_mig1.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'S:/Unreal/Project/Migrate_bike/02/Bike_mig1/Binaries/HTML5/Bike_mig1.data';
-    var REMOTE_PACKAGE_BASE = 'Bike_mig1.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/scl/fi/1qqwkh4gofj1hhzhsmxi8/Bike_mig1.data?rlkey=ieydx6z4nxoauoc72ihslxwat&dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/scl/fi/1qqwkh4gofj1hhzhsmxi8/Bike_mig1.data?rlkey=ieydx6z4nxoauoc72ihslxwat&dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
